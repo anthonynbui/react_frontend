a81f2181c2361e5142a9aa28b0224454c58221b4
Anthony Bui <anthonynbui99@gmail.com>
2022-09-29 10:54:01 -0700

    code

--- src/MyApp.js
+++ src/MyApp.js
@@ -15,6 +15,24 @@ function MyApp() {
      });
  }, [] );
 
+ async function makePostCall(person){
+  try {
+     const response = await axios.post('http://localhost:5000/users', person);
+     return response;
+  }
+  catch (error) {
+     console.log(error);
+     return false;
+  }
+}
+
+function updateList(person) { 
+  makePostCall(person).then( result => {
+  if (result && result.status === 201)
+     setCharacters([...characters, person] );
+  });
+}
+
 async function fetchAll(){
    try {
       const response = await axios.get('http://localhost:5000/users');
